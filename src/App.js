20bf1a7983f6fa190b59efe3157d79019754dd79
Sampsa <sampadev@gmail.com>
2020-09-07 13:50:02 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const nextLaunchUrl = "https://api.spacexdata.com/v3/launches";
+  const [nextLaunch, setNextLaunch] = useState(null);
+
+  useEffect(() => {
+    fetch(nextLaunchUrl)
+      .then(res => res.json())
+      .then(json => {
+        setNextLaunch(json)
+        console.log(json)
+      })
+  }, []);
+    
+  // return loading screen
+  if(nextLaunch == null){
+    return(
+      <div>
+        <h1> Loading... </h1>
+      </div>
+    )
+  }
+
+  // return main page 
+  else{
+    return (   
+      <div>
+        <h1> API calls with React Hooks </h1>
+      </div>
+    );
+  } 
 }
 
 export default App;
